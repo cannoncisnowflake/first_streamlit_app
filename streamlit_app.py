5484876e77aef58b76ce068058a427ab5fb3af4a
cannoncisnowflake <111383959+cannoncisnowflake@users.noreply.github.com>
2022-08-16 14:23:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rooket Smoothie')
-streamlit.title('Hard Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rooket Smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
