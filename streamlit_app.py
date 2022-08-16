b5363b5f9f5e07b62d521cc05d6b10031ef67d13
cannoncisnowflake <111383959+cannoncisnowflake@users.noreply.github.com>
2022-08-16 14:37:33 -0400

    Update streamlit_app.py
    Add Emogis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rooket Smoothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗  Kale, Spinach & Rooket Smoothie')
+streamlit.text('🐔  Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞  Hard Boiled Free-Range Egg')
 
